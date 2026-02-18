833f0895cfa0a8328e6b9cb07574cb69dcab65ee
Krish-beep738 <jagan101299@gmail.com>
2026-02-18 17:21:58 +0530

    Commit message

---
+++ Assinment 1 ( Perform Basic Mathematical Operations).py
@@ -0,0 +1,12 @@
+first_number = int(input("Enter the first number: "))
+Second_number = int(input("Enter the second number: "))
+
+Addition = (first_number + Second_number)
+Subtraction = (first_number - Second_number)
+Multiplication = (first_number * Second_number)
+Division = (first_number / Second_number)
+
+print("Addition =",Addition)
+print("Subtraction =",Subtraction)
+print("Multiplication =",Multiplication)
+print("Division =",Division)
